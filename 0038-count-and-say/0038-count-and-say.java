58dc4e655d17dacb16c0d8d142d96b957ab65ae4
vaibhavpriyatosh <90557665+vaibhavpriyatosh@users.noreply.github.com>
2022-10-18 18:05:55 +0530

    Time: 52 ms (20.64%), Space: 60.8 MB (20.62%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,33 @@
+class Solution {
+    public static String cntsy(int num){
+        if(num==1)
+            return "1";
+
+        String s=cntsy(num-1);
+        int n=s.length();
+
+        int k=0;
+        String neww="";
+
+        for(int i=0;i<n;i++){
+            char c1=s.charAt(i);
+            k=0;
+
+            while(i<n && c1==s.charAt(i)){
+                k++;i++;
+            }
+
+            i--;
+
+            neww=neww+k+""+c1;
+
+        }
+
+        return neww;
+    
+
+    }
+    public String countAndSay(int n) {
+      return cntsy(n);  
+    }
+}
