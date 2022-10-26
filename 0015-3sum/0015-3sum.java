3223d88306de714f183f7695a13f83c322836b00
vaibhavpriyatosh <90557665+vaibhavpriyatosh@users.noreply.github.com>
2022-10-26 06:43:33 +0530

    Time: 496 ms (24.54%), Space: 58.8 MB (64.34%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int len=nums.length;
+        Arrays.sort(nums);
+        
+        List<List<Integer>> ll=new LinkedList<>();
+        if(len<3)return ll;int previ=Integer.MAX_VALUE,prevj=Integer.MAX_VALUE;
+        for(int i=0;i<len-1;i++){
+            if(previ==nums[i])continue;
+            for(int j=i+1;j<len;j++){
+                if(prevj==nums[j])continue;
+                int temp=nums[i]+nums[j];
+                temp*=-1;
+                int index=finding(nums,j+1,len-1,temp);
+                if(index!=-1)ll.add(Arrays.asList(nums[i],nums[j],nums[index]));
+                prevj=nums[j];
+            }
+            previ=nums[i];
+        }
+        return ll;
+    }
+    public static int finding(int []nums,int start,int end,int find){
+        if(start>end)return -1;
+        int mid=(start+end)/2;
+        if(nums[mid]==find)return mid;
+        else if(nums[mid]<find)return finding(nums,mid+1,end,find);
+        else return finding(nums,start,mid-1,find);
+    }
+}
